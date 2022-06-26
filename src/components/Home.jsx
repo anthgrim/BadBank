0fcef11a6bd76b07fc0eb0c96f59976c0c6d2195
Kevin Grimaldi <anthgrim@icloud.com>
2022-06-25 20:48:32 -0400

    Added card to home component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,25 @@
+import bankImage from "../images/jason-dent-3wPJxh-piRw-unsplash.jpg";
+
 const Home = () => {
+  const customStyles = {
+    width: "18rem",
+    padding: "10px",
+    zIndex: "-1",
+  };
+
   return (
     <>
-      <h1>Home</h1>
+      <div className="card" style={customStyles}>
+        <h5 className="card-title">Bad Bank</h5>
+        <img src={bankImage} className="card-img-top" alt="..." />
+        <div className="card-body">
+          <p className="card-text">
+            Welcome! to the most secure bank there is in the virtual world. We
+            promise you that we will
+            <span className="bad-policy-practice">not</span> secure your data.
+          </p>
+        </div>
+      </div>
     </>
   );
 };
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,10 +2,13 @@ import { Outlet } from "react-router-dom";
 import Navbar from "./Navbar";
 
 const Layout = () => {
+  const customStyles = {
+    paddingTop: "10px",
+  };
   return (
     <>
       <Navbar />
-      <div className="container">
+      <div className="container" style={customStyles}>
         <Outlet />
       </div>
     </>
