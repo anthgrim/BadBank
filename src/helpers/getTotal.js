d18576053babdf1ecdbdf51cc40f90fd35c33953
Kevin Grimaldi <anthgrim@icloud.com>
2022-07-13 23:48:13 -0400

    removing comment

--- src/helpers/getTotal.js
+++ src/helpers/getTotal.js
@@ -3,8 +3,6 @@ const getTotal = (targetType, data) => {
   const targetArray = data.filter((record) => record.type === targetType);
   if (targetArray.length === 0) return 0;
   if (targetArray.length === 1) return targetArray[0].amount;
-  console.log(targetType);
-  console.log(targetArray);
   return targetArray.reduce((acc, obj) => acc + obj.amount, 0);
 };
 
